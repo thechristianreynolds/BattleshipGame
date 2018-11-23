09bc61ca1acbc825dc7aa3c2623f8871ef321012
akibby <alkibler95@gmail.com>
2018-11-23 11:53:47 -0600

    Set resizability to false

--- BSWindow.java
+++ BSWindow.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class BSWindow extends JFrame {
 
     private int winWidth = 800;
-    private int winHeight = 600;
+    private int winHeight = 450;
     BSDisplay display;
     BSLogic game;
     int cellAcross = 10;
@@ -15,6 +15,7 @@ public class BSWindow extends JFrame {
         this.setTitle("Battleship!");
         this.setSize(winWidth, winHeight);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
 
         game = new BSLogic(cellDown, cellAcross);
         display = new BSDisplay(game);
